adfcf29e56c77c86db8781192c977470c1366592
Thang0226 <thang.nd0226@gmail.com>
2025-02-04 13:58:10 +0700

    can show products

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ProductsList from "./components/ProductList";
+
+function App() {
+  return (
+    <div>
+      <h2>Shopping Cart</h2>
+      <hr />
+      <ProductsList />
+      <hr />
+    </div>
+  );
+}
+
+export default App;
